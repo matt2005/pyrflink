5591a01ac9dc85e9035138cfe6f7addd9bf15b2a
matt <matthilton2005@gmail.com>
2016-08-31 19:26:44 +0100

    readline testing

--- rflink/rflink.py
+++ rflink/rflink.py
@@ -155,7 +155,7 @@ class SerialGateway(Gateway, threading.Thread):
             try:
                 line = None
                 line = repr(self.readlineCR())
-                if !(line == None):
+                if not (line == None):
                     continue
             except serial.SerialException:
                 LOGGER.exception('Serial exception')
